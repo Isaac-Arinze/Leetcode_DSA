85976251839cc7570693e903c5e450f5a1646766
Sky Tech <Isaac-Arinze>
2024-10-26 22:04:44 +0100

    Additiion of numbers

---
+++ src/main/java/org/example/Sum.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Sum {
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(6, 2));
+
+    }
+}
