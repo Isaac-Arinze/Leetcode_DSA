661413fe938a8923229ac7a80bb50952a44c2183
Sky Tech <Isaac-Arinze>
2024-10-26 22:42:52 +0100

    Working

--- src/main/java/org/example/Sum.java
+++ src/main/java/org/example/Sum.java
@@ -6,8 +6,26 @@ public class Sum {
         return a + b;
     }
 
+    public static void announce (){
+        System.out.println("Announcement");
+    }
+
+    public static String bark(String woo){
+                return woo;
+    }
+
+    public static double withdraw (double amount){
+        return amount;
+    }
+
     public static void main(String[] args) {
         System.out.println(sum(6, 2));
+        announce();
+        System.out.println(bark("Barking continously"));;
+
+        System.out.println("**************************************");
+        System.out.print("You have successfully transferred the sum of");
+        System.out.print(withdraw(2000));
 
     }
 }
